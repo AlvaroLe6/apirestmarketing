d0ce1a14525a061f29accc51cab536e8c8d6b5a3
AlvaroLe6 <alvaro_147@hotmail.es>
2024-06-28 18:32:08 -0400

    28/07

--- app.js
+++ app.js
@@ -110,9 +110,9 @@ app.get('/api/grado_academico',(req,res) =>{
     })
 })
 
-// mostrar todos las ciudades de recidencia
-app.get('/api/ciudad_recidencia',(req,res) =>{
-    conexion.query('SELECT * FROM marketing_db.ciudad_recidencia;', (error, filas)=>{
+// mostrar todos las ciudades de residencia
+app.get('/api/ciudad_residencia',(req,res) =>{
+    conexion.query('SELECT * FROM marketing_db.ciudad_residencia;', (error, filas)=>{
         if(error){
             throw error;
         }else{
@@ -120,6 +120,101 @@ app.get('/api/ciudad_recidencia',(req,res) =>{
         }
     })
 })
+
+// Muestra todas las inscripciones con detalles de persona y programa
+app.get('/api/inscripciones_persona', (req, res) => {
+    const sql = `
+    SELECT 
+            p.num_doc,
+            p.nombres,
+            p.apellidos,
+            p.genero,
+            p.fecha_nac,
+            p.domicilio,
+            p.telefono,
+            p.correo,
+            cr.nombre_ciudad AS ciudad_residencia,
+            ga.grado_academico AS grado_academico,
+            ie.nombre_institucion AS institucion_egreso,
+            pr.nombre_profesion AS profesion,
+            pg.Nombre AS programa,
+            ins.fecha_reg,
+            ins.cod_asesor
+    FROM 
+        marketing_db.persona p
+    JOIN 
+        marketing_db.inscripciones ins ON p.cod_persona = ins.cod_persona
+    JOIN 
+        marketing_db.ciudad_residencia cr ON p.cod_ciudad_r = cr.cod_ciudad_r
+    JOIN 
+        marketing_db.programas pg ON ins.Cod_Programa = pg.Cod_Programa
+    JOIN 
+        marketing_db.grado_academico ga ON p.cod_grado_a = ga.cod_grado_a
+    JOIN 
+        marketing_db.institucion_egreso ie ON p.cod_institucion_e = ie.cod_institucion_e
+    JOIN 
+        marketing_db.profesiones pr ON p.cod_profesion = pr.cod_profesion;
+    `;
+    conexion.query(sql, (error, filas) => {
+        if (error) {
+            console.error('Error al consultar la base de datos:', error);
+            return res.status(500).send('Error interno del servidor');
+        } else {
+            res.send(filas);
+        }
+    });
+});
+
+// Muestra todas las inscripciones por programa con detalles de persona
+app.get('/api/inscripciones_por_programa', (req, res) => {
+    const codPrograma = req.query.Cod_Programa;
+    if (!codPrograma) {
+        return res.status(400).send('Falta el parámetro Cod_Programa');
+    }
+    const sql = `
+    SELECT 
+            p.num_doc,
+            p.nombres,
+            p.apellidos,
+            p.genero,
+            p.fecha_nac,
+            p.domicilio,
+            p.telefono,
+            p.correo,
+            cr.nombre_ciudad AS ciudad_residencia,
+            ga.grado_academico AS grado_academico,
+            ie.nombre_institucion AS institucion_egreso,
+            pr.nombre_profesion AS profesion,
+            pg.Nombre AS programa,
+            ins.fecha_reg,
+            ins.cod_asesor
+    FROM 
+        marketing_db.persona p
+    JOIN 
+        marketing_db.inscripciones ins ON p.cod_persona = ins.cod_persona
+    JOIN 
+        marketing_db.ciudad_residencia cr ON p.cod_ciudad_r = cr.cod_ciudad_r
+    JOIN 
+        marketing_db.programas pg ON ins.Cod_Programa = pg.Cod_Programa
+    JOIN 
+        marketing_db.grado_academico ga ON p.cod_grado_a = ga.cod_grado_a
+    JOIN 
+        marketing_db.institucion_egreso ie ON p.cod_institucion_e = ie.cod_institucion_e
+    JOIN 
+        marketing_db.profesiones pr ON p.cod_profesion = pr.cod_profesion
+         where pg.Cod_Programa = ?;
+    `;
+    conexion.query(sql, [codPrograma], (error, filas) => { // Pasar codPrograma como parámetro
+
+        if (error) {
+            console.error('Error al consultar la base de datos:', error);
+            return res.status(500).send('Error interno del servidor');
+        } else {
+            res.send(filas);
+        }
+    });
+});
+
 
 // Subir los archivos
 // Configuración de Multer(ruta del archivo)
