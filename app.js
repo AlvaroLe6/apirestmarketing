a492046d6cfbed12a4bf26bc61e8413a68635cf6
AlvaroLe6 <alvaro_147@hotmail.es>
2024-08-11 22:21:24 -0400

    11/08/2024

--- app.js
+++ app.js
@@ -4,16 +4,18 @@ var cors = require('cors');
 var multer = require('multer');
 var path = require('path');
 var WebSocket = require('ws');
-
 var app = express();
+var shortid = require('shortid');
+
 app.use(express.json());
 app.use(cors());
 
 
-
 //  Especiica el origen exacto (recomendado para producción)
 var corsOptions = {
-    origin: 'http://localhost:5173',  // Asegúrate que este sea el puerto y protocolo donde se ejecuta tu cliente Vue.js
+    //origin: 'https://esammarketingapi-36cc8f6bb2a2.herokuapp.com',  // Asegúrate que este sea el puerto y protocolo donde se ejecuta tu cliente Vue.js
+   //origin: 'https://mktlapaz.esam.edu.bo/',
+    origin: '*',
     optionsSuccessStatus: 200
 };
 
@@ -21,14 +23,21 @@ app.use(cors(corsOptions));
 
 // parametros para la conexion
 var conexion = mysql.createConnection({
-    host: 'localhost',
+    /*host: 'localhost',
     user:'root',
     password:'',
-    database:'marketing_db'
+    database:'marketing_db'*/
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+
 });
 
-
-const wss = new WebSocket.Server({ port: 8080 });
+const wsPort = process.env.WS_PORT || 8080;
+const wss = new WebSocket.Server({ port: wsPort });
+//const wss = new WebSocket.Server({ port: 8080 });
 wss.on('connection', ws => {
     ws.on('message', message => {
       // Cuando se recibe un mensaje, reenviar a todos los clientes conectados
@@ -52,12 +61,54 @@ if(error)
 
 
 app.get('/', function(req, res){
-    res.send('ruta INICIO')
+    res.send('ruta INICIOnueva ')
 })
 
 // mostrar todos las programas
 app.get('/api/programas',(req,res) =>{
-    conexion.query('SELECT * FROM marketing_db.programas;', (error, filas)=>{
+    const query = `
+    SELECT 
+            p.Cod_Programa AS Cod_Programa, 
+            p.Nombre AS Nombre, 
+           a.Nombre_Area AS Area, 
+           e.estado AS Estado
+    FROM marketing_db.programas p
+    JOIN marketing_db.area a ON p.Cod_Area = a.Cod_Area
+    JOIN marketing_db.estado_programa e ON p.cod_estado_p = e.cod_estado_p;
+    `;
+    
+    conexion.query(query, (error, results) => {
+        if (error) {
+            console.error('Error al ejecutar la consulta:', error);
+            return res.status(500).send('Error interno del servidor');
+        }
+        res.send(results);
+    });
+});
+
+// Numero de inscritos por programa
+app.get('/api/inscritos_por_programa', (req, res) => {
+    const query = `
+        SELECT 
+            cod_programa, 
+            COUNT(*) as num_inscritos 
+        FROM 
+            marketing_db.inscripciones
+        GROUP BY 
+            cod_programa
+    `;
+    conexion.query(query, (error, results) => {
+        if (error) {
+            console.error('Error al obtener el número de inscritos:', error);
+            return res.status(500).send('Error interno del servidor');
+        }
+        res.send(results);
+    });
+});
+
+// mostrar todas las areas
+app.get('/api/areas',(req,res) =>{
+    conexion.query('SELECT * FROM marketing_db.area;', (error, filas)=>{
         if(error){
             throw error;
         }else{
@@ -339,9 +390,146 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads'), {
     }
 }));
 
+// Generar un enlace personalizado
+app.post('/api/generar-url-pers', (req, res) => {
+    const { codPrograma, nombre, codAsesor } = req.body;
+
+    // Primero, verifica si ya existe un enlace para el programa y asesor
+    const checkQuery = 'SELECT short_id FROM url_mapping WHERE cod_programa = ? AND cod_asesor = ?';
+    conexion.query(checkQuery, [codPrograma, codAsesor], (checkError, checkResults) => {
+        if (checkError) {
+            console.error('Error al verificar la URL en la base de datos:', checkError);
+            return res.status(500).send('Error interno del servidor');
+        }
+
+        // Si ya existe un enlace, devolver el existente
+        if (checkResults.length > 0) {
+            const existingShortId = checkResults[0].short_id;
+            return res.send({ shortUrl: `https://mktlapaz.esam.edu.bo/${existingShortId}` });
+        }
+
+        // Si no existe, genera uno nuevo
+        const customId = shortid.generate();
+        const insertQuery = 'INSERT INTO url_mapping (short_id, cod_programa, nombre, cod_asesor) VALUES (?, ?, ?, ?)';
+        conexion.query(insertQuery, [customId, codPrograma, nombre, codAsesor], (insertError, insertResults) => {
+            if (insertError) {
+                console.error('Error al guardar la URL en la base de datos:', insertError);
+                return res.status(500).send('Error interno del servidor');
+            }
+            res.send({ shortUrl: `https://mktlapaz.esam.edu.bo/${customId}` });
+        });
+    });
+});
+
+
+// Redirigir al enlace completo
+app.get('/:shortId', (req, res) => {
+    const shortId = req.params.shortId;
+
+    const query = 'SELECT cod_programa, nombre, cod_asesor FROM url_mapping WHERE short_id = ?';
+    conexion.query(query, [shortId], (error, results) => {
+        if (error) {
+            console.error('Error al buscar la URL en la base de datos:', error);
+            return res.status(500).send('Error interno del servidor');
+        }
+        if (results.length > 0) {
+            const { cod_programa, nombre, cod_asesor } = results[0];
+            res.redirect(`/formulario-inscripcion?Cod_Programa=${cod_programa}&Nombre=${encodeURIComponent(nombre)}&Cod_Asesor=${cod_asesor}`);
+        } else {
+            res.status(404).send('URL no encontrada');
+        }
+    });
+});
+
+app.get('/api/:shortId', (req, res) => {
+    const shortId = req.params.shortId;
+
+    const query = `SELECT 
+    u.cod_programa, 
+    u.nombre, 
+    u.cod_asesor, 
+    a.nombre AS nombre_asesor
+    FROM 
+        marketing_db.url_mapping u
+    JOIN 
+        marketing_db.asesores a
+    ON 
+        u.cod_asesor = a.cod_asesor
+    WHERE 
+        u.short_id = ?;`;
+    conexion.query(query, [shortId], (error, results) => {
+        if (error) {
+            console.error('Error al buscar la URL en la base de datos:', error);
+            return res.status(500).send('Error interno del servidor');
+        }
+        if (results.length > 0) {
+            const { cod_programa, nombre, cod_asesor,nombre_asesor } = results[0];
+            res.json({ cod_programa, nombre, cod_asesor, nombre_asesor }); // Devuelve los datos en formato JSON
+        } else {
+            res.status(404).send('URL no encontrada');
+        }
+    });
+});
+
+app.put('/api/update_asesores/:cod_asesor', (req, res) => {
+    const codAsesor = req.params.cod_asesor;
+    const {
+      nombre, estado, cargo, acceso_sis, correo_electronico, ci, extension, fecha_nac,
+      domicilio, ciudad_rec, fecha_ingreso, entidad_financiera, num_celular, num_celular_P,
+      equipo, tipo_contrato
+    } = req.body;
+  
+    const sql = `
+      UPDATE marketing_db.asesores
+      SET nombre = ?, estado = ?, cargo = ?, acceso_sis = ?, correo_electronico = ?, ci = ?, extension = ?, fecha_nac = ?, domicilio = ?, ciudad_rec = ?, fecha_ingreso = ?, entidad_financiera = ?, num_celular = ?, num_celular_P = ?, equipo = ?, tipo_contrato = ?
+      WHERE cod_asesor = ?;
+    `;
+  
+    const values = [nombre, estado, cargo, acceso_sis, correo_electronico, ci, extension, fecha_nac, domicilio, ciudad_rec, fecha_ingreso, entidad_financiera, num_celular, num_celular_P, equipo, tipo_contrato, codAsesor];
+  
+    conexion.query(sql, values, (error, results) => {
+      if (error) {
+        console.error('Error al actualizar el asesor:', error);
+        return res.status(500).send('Error interno del servidor');
+      }
+  
+      res.send('Asesor actualizado exitosamente');
+    });
+  });
+  
+app.put('/api/asesores/:cod_asesor', (req, res) => {
+    const codAsesor = req.params.cod_asesor;
+    const { estado } = req.body;
+  
+    const sql = `
+      UPDATE marketing_db.asesores
+      SET estado = ?
+      WHERE cod_asesor = ?;
+    `;
+  
+    conexion.query(sql, [estado, codAsesor], (error, results) => {
+      if (error) {
+        console.error('Error al actualizar el estado:', error);
+        return res.status(500).send('Error interno del servidor');
+      }
+  
+      if (results.affectedRows === 0) {
+        return res.status(404).send('Asesor no encontrado');
+      }
+  
+      res.send('Estado actualizado exitosamente');
+    });
+  });
+  
+/*
 const puerto = process.env.PUERTO || 3000;
 
 app.listen(puerto,function()
 {
     console.log("Servidor Ok en puerto:" + puerto);
+});*/
+const port = process.env.PORT || 3000;
+
+app.listen(port, function() {
+    console.log("Servidor Ok en puerto:" + port);
 });
